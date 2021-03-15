77a9120d6d046d9aa8857d3ec4cd552082960821
Alexis <79303925+mYiShadows@users.noreply.github.com>
2021-03-15 15:52:55 +0100

    Add files via upload

---
+++ JsMorpion.js
@@ -0,0 +1,11 @@
+// Aspect croix
+
+var ratioCroix = 0.7 ;                         // Taille de la croix
+var epaisseurCroix = 1 ;                       // Epaisseur du trait de la croix
+var couleurCroix = "blue";                     // Couleur de la croix
+
+// Aspect rond
+
+var ratioRond = 0.7 ;
+var epaisseurRond = 1 ;
+var couleurRond = "red";
